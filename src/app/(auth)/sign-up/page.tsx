5873e5266d573c8b27e52270154ff86b6f62873c
umarbashirr <umarbashir93@outlook.com>
2024-05-25 23:18:44 +0530

    auth page created with layout

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,41 @@
+import Logo from "@/components/logo";
+import SignupForm from "../_components/signup-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 min-h-screen w-full max-w-screen-sm mx-auto p-6">
+      <Logo />
+      <Card>
+        <CardHeader>
+          <CardTitle>Create your account</CardTitle>
+          <CardDescription>
+            Join us now and start managing your business with us
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignupForm />
+        </CardContent>
+        <CardFooter className="text-sm">
+          Already have an account?{" "}
+          <Link
+            href="/sign-in"
+            className="text-indigo-600 hover:text-indigo-500"
+          >
+            Sign-in
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Page;
